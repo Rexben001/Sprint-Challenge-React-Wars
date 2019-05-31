229ba3e1b674b466b4eb17d87556aa27d9addc4f
Rexben <rexben.rb@gmail.com>
2019-05-31 09:42:09 +0100

    create startwars and style them

---
+++ starwars/src/components/StarWars.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import uuid from 'uuid';
+import './StarWars.css';
+
+import StarWar from './StarWar';
+
+const starWars = (props) => (
+    props.starwarsChars.map(character => {
+       return <StarWar key={uuid()} characters= {character}/>
+    })
+);
+
+export default starWars;
